7ed22bb9eff39412ec12f5bdc45e4d8caddfec17
Zander-M <sherlingford@yahoo.com>
2019-10-08 10:50:47 -0400

    shadow complete

---
+++ Assignment_1/build/src/inside/CMakeFiles/point_in_polygon.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/point_in_polygon.dir/main.cpp.o"
+  "../../point_in_polygon.pdb"
+  "../../point_in_polygon"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/point_in_polygon.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
