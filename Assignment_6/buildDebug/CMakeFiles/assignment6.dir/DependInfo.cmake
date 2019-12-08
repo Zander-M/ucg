a79e92a94a95bb35bf8c32544c3243973fe64334
Zander-M <sherlingford@yahoo.com>
2019-12-08 12:36:05 -0500

    Assignment 6 key 1

---
+++ Assignment_6/buildDebug/CMakeFiles/assignment6.dir/DependInfo.cmake
@@ -0,0 +1,31 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/zdrm/Desktop/Fall19/CG/ucg/Assignment_6/src/helpers.cpp" "/home/zdrm/Desktop/Fall19/CG/ucg/Assignment_6/buildDebug/CMakeFiles/assignment6.dir/src/helpers.cpp.o"
+  "/home/zdrm/Desktop/Fall19/CG/ucg/Assignment_6/src/main.cpp" "/home/zdrm/Desktop/Fall19/CG/ucg/Assignment_6/buildDebug/CMakeFiles/assignment6.dir/src/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "DATA_DIR=\"/home/zdrm/Desktop/Fall19/CG/ucg/Assignment_6/data/\""
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../../ext/eigen"
+  "/home/zdrm/Desktop/Fall19/CG/ucg/ext/glfw/include"
+  "/home/zdrm/Desktop/Fall19/CG/ucg/ext/glad/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/zdrm/Desktop/Fall19/CG/ucg/Assignment_6/buildDebug/glfw/src/CMakeFiles/glfw.dir/DependInfo.cmake"
+  "/home/zdrm/Desktop/Fall19/CG/ucg/Assignment_6/buildDebug/glad/CMakeFiles/glad.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ Assignment_6/buildDebug/CMakeFiles/assignment6.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/assignment6.dir/src/main.cpp.o"
+  "CMakeFiles/assignment6.dir/src/helpers.cpp.o"
+  "assignment6.pdb"
+  "assignment6"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/assignment6.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
