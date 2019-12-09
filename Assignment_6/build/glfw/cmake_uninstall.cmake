0fbddf5aabd0563ed5178d7bb379c2e841794155
Zander-M <sherlingford@yahoo.com>
2019-12-09 04:18:30 -0500

    Transform done, rendering not correct

---
+++ Assignment_6/build/glfw/cmake_uninstall.cmake
@@ -0,0 +1,29 @@
+
+if (NOT EXISTS "/home/zdrm/Desktop/Fall19/CG/ucg/Assignment_6/build/glfw/install_manifest.txt")
+  message(FATAL_ERROR "Cannot find install manifest: \"/home/zdrm/Desktop/Fall19/CG/ucg/Assignment_6/build/glfw/install_manifest.txt\"")
+endif()
+
+file(READ "/home/zdrm/Desktop/Fall19/CG/ucg/Assignment_6/build/glfw/install_manifest.txt" files)
+string(REGEX REPLACE "\n" ";" files "${files}")
+
+foreach (file ${files})
+  message(STATUS "Uninstalling \"$ENV{DESTDIR}${file}\"")
+  if (EXISTS "$ENV{DESTDIR}${file}")
+    exec_program("/usr/bin/cmake" ARGS "-E remove \"$ENV{DESTDIR}${file}\""
+                 OUTPUT_VARIABLE rm_out
+                 RETURN_VALUE rm_retval)
+    if (NOT "${rm_retval}" STREQUAL 0)
+      MESSAGE(FATAL_ERROR "Problem when removing \"$ENV{DESTDIR}${file}\"")
+    endif()
+  elseif (IS_SYMLINK "$ENV{DESTDIR}${file}")
+    EXEC_PROGRAM("/usr/bin/cmake" ARGS "-E remove \"$ENV{DESTDIR}${file}\""
+                 OUTPUT_VARIABLE rm_out
+                 RETURN_VALUE rm_retval)
+    if (NOT "${rm_retval}" STREQUAL 0)
+      message(FATAL_ERROR "Problem when removing symlink \"$ENV{DESTDIR}${file}\"")
+    endif()
+  else()
+    message(STATUS "File \"$ENV{DESTDIR}${file}\" does not exist.")
+  endif()
+endforeach()
+
